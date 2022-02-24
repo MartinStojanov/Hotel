5333454055a6239d4057deab97c4d2d7def79b27
taskovikj <branislav.takovikj@gmail.com>
2022-02-24 21:07:54 +0100

    git problems resolved?

--- src/main/java/com/example/demo/controllers/EmployeesController.java
+++ src/main/java/com/example/demo/controllers/EmployeesController.java
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
-
-//Test 4 GIT-HUB
 @Controller
 public class EmployeesController {
     private final EmployeeService employeeService;
@@ -23,7 +22,7 @@ public class EmployeesController {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/listEmployees")
+    @GetMapping("/employees")
     public String listEmployees(Model model){
 
         List<Employee> employeeList = this.employeeService.listAll();
@@ -31,12 +30,9 @@ public class EmployeesController {
         return "listEmployees";
     }
 
-    //eden Get Mapping so input forma za addEmployee
-    //TODO:
-    //@GetMapping
 
 
-    @PostMapping("/addEmployee")
+    @PostMapping("/employees/add")
     public String addEmployee(@RequestParam String name,
                               @RequestParam String surname,
                               @RequestParam String EMBG,
@@ -44,21 +40,46 @@ public class EmployeesController {
                               @RequestParam float salary,
                               @RequestParam Role position){
         this.employeeService.create(name,surname,EMBG,email,salary,position);
-        return "redirect:/listEmployees";//da se smeni Strana so nov vraboten
+        return "editEmployee";//da se smeni Strana so nov vraboten
+    }
+
+    @GetMapping("/employee/{id}/edit")
+    public String showEdit(@PathVariable Long id, Model model) {
+        Employee emp = this.employeeService.findById(id);
+        List<Role> roles = new ArrayList<>();
+        Role a = Role.chef;
+        Role b = Role.cleaner;
+        Role c = Role.director;
+        Role d = Role.reception;
+        Role e = Role.hostess;
+        Role f = Role.manager;
+        Role g = Role.waiter;
+        roles.add(a);
+        roles.add(b);
+        roles.add(c);
+        roles.add(d);
+        roles.add(e);
+        roles.add(g);
+        roles.add(f);
+
+        model.addAttribute("emp",emp);
+        model.addAttribute("roles",roles);
+
+        return "editEmployee";
     }
 
     @PostMapping("/employee/{id}")
-    public String updateEmployee(@PathVariable Long id,
-                                 @RequestParam String name,
-                                 @RequestParam String surname,
-                                 @RequestParam String EMBG,
-                                 @RequestParam String email,
-                                 @RequestParam float salary,
-                                 @RequestParam Role position){
-        this.employeeService.update(id,name,surname,EMBG,email,salary,position);
-        return "redirect:/employees";//da se smeni Strana so updated rabotnik
-
+    public String update(@PathVariable Long id,
+                         @RequestParam String name,
+                         @RequestParam String surname,
+                         @RequestParam String EMBG,
+                         @RequestParam String email,
+                         @RequestParam float salary,
+                         @RequestParam Role position) {
+        this.employeeService.update(id, name, surname, EMBG, email,salary,position);
+        return "redirect:/employees";
     }
+
     @PostMapping("/employee/{id}/delete")
     public String delete(@PathVariable Long id) {
         this.employeeService.delete(id);
