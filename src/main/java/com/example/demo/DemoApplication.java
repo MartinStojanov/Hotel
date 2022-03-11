b91c8ae5f3b7152f0886b6a31ca6d84f3d772908
taskovikj <branislav.takovikj@gmail.com>
2022-03-11 11:24:23 +0100

    email sender files uploaded

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.model.Employee;
-import com.example.demo.model.Role;
-import com.example.demo.service.impl.EmailSenderService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.event.EventListener;
 
 import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.util.Properties;
 
 @SpringBootApplication
 public class DemoApplication {
