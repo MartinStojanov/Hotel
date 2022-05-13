0398481859faf80df485c014852de56634ab346e
stojanchevski <nikolastojancevski@yahoo.com>
2022-05-13 15:32:33 +0200

    CSV Import IMPLEMENTED!

--- src/main/java/com/example/demo/controllers/EmployeesController.java
+++ src/main/java/com/example/demo/controllers/EmployeesController.java
@@ -1,24 +1,28 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.CsvUtils;
 import com.example.demo.model.Employee;
 import com.example.demo.model.Role;
+import com.example.demo.repository.EmployeeRepo;
 import com.example.demo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Controller
 public class EmployeesController {
     private final EmployeeService employeeService;
+    private final EmployeeRepo employeeRepo;
 
 
-    public EmployeesController(EmployeeService employeeService) {
+    public EmployeesController(EmployeeService employeeService, EmployeeRepo employeeRepo) {
         this.employeeService = employeeService;
+        this.employeeRepo = employeeRepo;
     }
 
     @GetMapping("/employees")
@@ -75,5 +79,25 @@ public class EmployeesController {
         this.employeeService.delete(id);
         return "redirect:/employees"; // da se smeni strana so izbrisan rabotnik
     }
+
+    @GetMapping("/uploadPage")
+    public String getUploadPage(){
+        return "uploadPage";
+    }
+
+    @PostMapping(value = "/upload", consumes = "text/csv")
+    public String uploadSimple(@RequestBody InputStream body) throws IOException {
+        employeeRepo.saveAll(CsvUtils.read(Employee.class, body));
+
+        return "redirect:/employees";
+    }
+
+    @PostMapping(value = "/upload", consumes = "multipart/form-data")
+    public String uploadMultipart(@RequestParam("file") MultipartFile file) throws IOException {
+        employeeRepo.saveAll(CsvUtils.read(Employee.class, file.getInputStream()));
+        return "redirect:/employees";
+    }
+
+
 
 }
